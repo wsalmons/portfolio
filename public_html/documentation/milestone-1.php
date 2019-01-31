d92669a883c985083336f409f7022699d81d2842
Wyatt Salmons <45742790+wsalmons@users.noreply.github.com>
2019-01-31 09:53:38 -0700

    Add files via upload

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>User Persona</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<ul>
+			<li><b>Purpose:</b> My PWP will help me display
+			my abilities, and competencies to prospective
+			employers.</li>
+			<li><b>Audience:</b> People in the technology field,
+			hiring managers, and employers will be viewing this
+			site.</li>
+			<li><b>Goal:</b> My  primary goal will be to gain full
+			time employment that utilizes my skills acquired in
+			this class.</li>
+		</ul>
+		<h1>Customer Persona</h1>
+		<img src="john-wick.jpg" alt="john-wick.jpg">
+		<ul>
+			<li><b>Name:</b> John Wick</li>
+			<li><b>Age:</b> 34</li>
+			<li><b>Occupation:</b> Hiring Manager at Tech Firm</li>
+			<li><b>Location:</b> Albuquerque, NM</li>
+			<li><b>Hobbies:</b> Tech, gaming, beer tasting</li>
+			<li><b>Owned Technology:</b>
+				<ul>
+					<li><b>Computer:</b> Lenovo 910, Windows 10</li>
+					<li><b>Mobile:</b> OnePlus 6t, Android 9.0</li>
+				</ul>
+				<li><b>Technology Comfort Level:</b> Very comfortable in
+				many aspects of coding, web design, and computer science</li>
+			<li><b>Goals:</b> Interested in hiring a junior developer</li>
+			<li><b>Technological Needs:</b> Easy access to candidate experience,
+			level of education, and examples of candidate capabilities and
+			competencies.</li>
+			<li><b>Frustrations:</b> Difficult to interpret portfolio design,
+			where information is not clearly laid out for the end user,
+			portfolios that do not accurately represent the developers skill set.</li>
+		</ul>
+		<h3>User Story</h3>
+		<p>John Wick has received a link to a junior developers portfolio
+		and would like to look at it to glean insight into this developers skill set.</p>
+	</body>
+</html>
