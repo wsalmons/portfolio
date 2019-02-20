5b2b29fa0d188f5703f6a0a67a453fba84ed7b7e
Wyatt Salmons <wyattsalmons@gmail.com>
2019-02-19 17:58:32 -0700

    rolled back to copy without animations, did not work on mobile.

--- js/script.js
+++ js/script.js
@@ -45,25 +45,3 @@ document.addEventListener('DOMContentLoaded',function(event){
 	// start the text animation
 	StartTextAnimation(0);
 });
-
-
-var $window = $(window);
-var $elem = $(".animation")
-
-function isScrolledIntoView($elem, $window) {
-	var docViewTop = $window.scrollTop();
-	var docViewBottom = docViewTop + $window.height();
-
-	var elemTop = $elem.offset().top;
-	var elemBottom = elemTop + $elem.height();
-
-	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-
-$(document).on("scroll", function () {
-	if (isScrolledIntoView($elem, $window)) {
-		$elem.addClass("animate")
-		console.log("now you see me");
-	}
-});
